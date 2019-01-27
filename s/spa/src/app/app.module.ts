ff8a062e583dbb34f2964efa9068c783a9369127
Randy Daddis <randy.daddis@outlook.com>
2019-01-27 13:57:39 -0500

    removed experimental layouts

--- s/spa/src/app/app.module.ts
+++ s/spa/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NestedCssGrids0Component } from './nested-css-grids-0/nested-css-grids-0.component';
 import { NestedCssGrids1Component } from './nested-css-grids-1/nested-css-grids-1.component';
-import { NestedCssGrids2Component } from './nested-css-grids-2/nested-css-grids-2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NestedCssGrids0Component,
-    NestedCssGrids1Component,
-    NestedCssGrids2Component
+    NestedCssGrids1Component
   ],
   imports: [
     BrowserModule,
